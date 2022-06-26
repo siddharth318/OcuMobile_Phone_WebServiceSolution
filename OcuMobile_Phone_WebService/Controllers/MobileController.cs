bad368e5f9a729d333333417479c1c1863235b1e
siddharth dwivedi <siddharthdwivedi318@gmail.com>
2022-06-26 23:36:52 +0530

    Add project files.

---
+++ OcuMobile_Phone_WebService/Controllers/MobileController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OcuMobile_Phone_WebService.Model;
+
+namespace OcuMobile_Phone_WebService.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MobileController : Controller
+    {
+        public List<Mobile> Get_Mobiles()
+        {
+            List<Mobile> mob = new List<Mobile>();
+            mob.Add(new Mobile() { MobileID = 1, MobileName = "OnePlus", Cost = 20000f });
+            mob.Add(new Mobile() { MobileID = 2, MobileName = "IPhone 13", Cost = 100000f });
+            mob.Add(new Mobile() { MobileID = 3, MobileName = "Samsung", Cost = 80000f });
+            return mob;
+
+        }
+    }
+}
---
+++ OcuMobile_Phone_WebService/Model/Mobile.cs
@@ -0,0 +1,11 @@
+﻿namespace OcuMobile_Phone_WebService.Model
+{
+    public class Mobile
+    {
+
+        public int MobileID { get; set; }
+        public string MobileName { get; set; }
+        public float Cost { get; set; }
+
+    }
+}
